e207ea0afb31f68408184298cfa36578bdb484c9
Thalys <sopraver80@gmail.com>
2024-09-19 18:47:07 -0300

    2

---
+++ Desafios Sala de Aula/1Desafio/1Desafio-q1.py
@@ -0,0 +1,14 @@
+conj = []
+qt= 0
+md = 0
+n = int(input("Digite N: "))
+
+for i in range (0,n-1):
+    conj.append(input("Digite o Valor do indice: "))
+    md = md + conj
+md = md/n
+print("UFC - Programação - 2024.1" )
+print("1ºDesafio")
+print("Elementos do Conjunto: ")
+
+
